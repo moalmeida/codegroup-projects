22b51bba4e4f8ea6a31a648308c4db9b6924f1a1
Mauro Almeida <moalmeida@koinosystems.com>
2024-04-15 14:30:19 -0300

    fix: sonar smells

--- src/test/java/br/com/codegroup/projects/ApplicationTest.java
+++ src/test/java/br/com/codegroup/projects/ApplicationTest.java
@@ -1,15 +1,21 @@
 package br.com.codegroup.projects;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ApplicationTest {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     void main() {
         Application.main(new String[]{});
+        assertNotNull(applicationContext, "The application context should have loaded");
     }
 }
