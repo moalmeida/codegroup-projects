66ac0761e50eace5fd654322d39c7f374a1632dc
Mauro Almeida <moalmeida@koinosystems.com>
2024-04-16 02:29:49 -0300

    refactor: changes request validation

--- src/main/java/br/com/codegroup/projects/domain/dto/PessoaRequest.java
+++ src/main/java/br/com/codegroup/projects/domain/dto/PessoaRequest.java
@@ -1,5 +1,7 @@
 package br.com.codegroup.projects.domain.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +13,9 @@ import lombok.Setter;
 @Setter
 public class PessoaRequest {
     private Long id;
+    @NotBlank
     private String nome;
+    @Pattern(regexp = "^$|^(\\d{2}/\\d{2}/\\d{4})$", message = "date format should be dd/mm/yyyy")
     private String dataNascimento;
     private String cpf;
     private Boolean gerente;
--- src/main/java/br/com/codegroup/projects/domain/dto/ProjetoRequest.java
+++ src/main/java/br/com/codegroup/projects/domain/dto/ProjetoRequest.java
@@ -1,11 +1,12 @@
 package br.com.codegroup.projects.domain.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.math.BigDecimal;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,12 +14,17 @@ import java.math.BigDecimal;
 @Setter
 public class ProjetoRequest {
     private Long id;
+    @NotBlank
     private String nome;
     private String descricao;
+    @Pattern(regexp = "^$|^(\\d{2}/\\d{2}/\\d{4})$", message = "date format should be dd/mm/yyyy")
     private String dataInicio;
+    @Pattern(regexp = "^$|^(\\d{2}/\\d{2}/\\d{4})$", message = "date format should be dd/mm/yyyy")
     private String dataFim;
+    @Pattern(regexp = "^$|^(\\d{2}/\\d{2}/\\d{4})$", message = "date format should be dd/mm/yyyy")
     private String dataPrevisaoFim;
     private String status;
+    @NotNull
     private Long gerente;
     private Double orcamento;
     private String risco;
